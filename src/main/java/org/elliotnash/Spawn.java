0f98417e4eb1dc36f1e03d9a5f71edb8c0e04bdd
elliotnash <elliotnash@users.noreply.github.com>
2020-08-06 09:15:53 -0700

    Initial commit

---
+++ src/main/java/org/elliotnash/Spawn.java
@@ -0,0 +1,18 @@
+package org.elliotnash;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Spawn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/org/elliotnash/SpawnCommand.java
@@ -0,0 +1,51 @@
+package org.elliotnash;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class SpawnCommand implements CommandExecutor, TabExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length==0){
+            spawnSelf(sender);
+        } else if (args.length==1){
+            spawnOther(sender, args[0]);
+        }
+        return false;
+    }
+    void spawnSelf(CommandSender sender){
+        if (sender instanceof Player){
+            Player player = (Player) sender;
+            player.teleport(player.getWorld().getSpawnLocation());
+            sender.sendMessage(ChatColor.AQUA+"You have been sent to spawn");
+        } else {
+            sender.sendMessage(ChatColor.RED+"This command must be run by a player");
+        }
+    }
+    void spawnOther(CommandSender sender, String playerName){
+
+        if (sender.hasPermission("spawn.other")) {
+            Player player = Bukkit.getPlayer(playerName);
+            if (player!=null){
+                player.teleport(player.getWorld().getSpawnLocation());
+                sender.sendMessage(ChatColor.RED+playerName+ChatColor.AQUA+" has been sent to spawn");
+            } else {
+                sender.sendMessage(ChatColor.RED+"This player is offline");
+            }
+        } else {
+            sender.sendMessage(ChatColor.RED+"You don't have permission to execute this command (spawn.other)");
+        }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return null;
+    }
+}
